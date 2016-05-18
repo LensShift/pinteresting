76523a0eecec5f9706295042d5686eade6aebe0a
Aparna Shrivastava <aparnasinbox@gmail.com>
2016-05-18 22:17:38 +0300

    Implemented Devise for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get "about" => "pages#about" # crates about path
   get "contactus" => "pages#contactus" #creates contactus path
